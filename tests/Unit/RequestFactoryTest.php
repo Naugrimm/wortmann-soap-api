afafdf937f6730b1b6fdfb9522dcc4df8964aa13
Erik Anders <5753604+Naugrimm@users.noreply.github.com>
2026-05-28 19:45:37 +0200

    test: start adding some tests to the package
    refs #0 @20m

---
+++ tests/Unit/RequestFactoryTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Naugrim\WortmannSoapApi\Tests\Unit;
+
+use DateTimeImmutable;
+use Illuminate\Config\Repository;
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Config;
+use InvalidArgumentException;
+use Naugrim\WortmannSoapApi\Bridge\RequestFactory;
+use Naugrim\WortmannSoapApi\Client\Type\GetServiceInfoBySerialNo;
+use Naugrim\WortmannSoapApi\Client\Type\GetServiceInfoByWarrantyEndingDate;
+use Naugrim\WortmannSoapApi\Client\Type\GetStockAndPriceInformationByProductId;
+use Naugrim\WortmannSoapApi\Client\Type\GetStockAndPriceInformationByProductIds;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(RequestFactory::class)]
+final class RequestFactoryTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $container = new Container();
+        $container->instance('config', new Repository([
+            'wortmann-soap-api' => [
+                'username' => 'fixture-user',
+                'password' => 'fixture-password',
+            ],
+        ]));
+
+        Config::setFacadeApplication($container);
+    }
+
+    protected function tearDown(): void
+    {
+        Config::clearResolvedInstances();
+        Config::setFacadeApplication(null);
+
+        parent::tearDown();
+    }
+
+    #[Test]
+    public function itCreatesSerialNumberRequestsWithConfiguredCredentials(): void
+    {
+        $request = RequestFactory::create(GetServiceInfoBySerialNo::class, 'SERIAL-123');
+
+        self::assertInstanceOf(GetServiceInfoBySerialNo::class, $request);
+        self::assertSame('fixture-user', $request->getUsername());
+        self::assertSame('fixture-password', $request->getPasswordHash());
+        self::assertSame('SERIAL-123', $request->getSerialNo());
+    }
+
+    #[Test]
+    public function itCreatesWarrantyEndingDateRequests(): void
+    {
+        $date = new DateTimeImmutable('2026-05-28');
+
+        $request = RequestFactory::create(GetServiceInfoByWarrantyEndingDate::class, $date);
+
+        self::assertInstanceOf(GetServiceInfoByWarrantyEndingDate::class, $request);
+        self::assertSame($date, $request->getWarrantyEndingDate());
+    }
+
+    #[Test]
+    public function itWrapsProductIdListsInArrayOfString(): void
+    {
+        $request = RequestFactory::create(GetStockAndPriceInformationByProductIds::class, ['A123', 'B456']);
+
+        self::assertInstanceOf(GetStockAndPriceInformationByProductIds::class, $request);
+        self::assertSame(['A123', 'B456'], $request->getProductIds()?->getString());
+    }
+
+    #[Test]
+    public function itRejectsInvalidStringParameters(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Request parameter #1 must be a string.');
+
+        RequestFactory::create(GetStockAndPriceInformationByProductId::class, 123);
+    }
+
+    #[Test]
+    public function itRejectsNonStringProductIdListItems(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Request parameter #1 must be a list of strings.');
+
+        RequestFactory::create(GetStockAndPriceInformationByProductIds::class, ['A123', 456]);
+    }
+}
